a61cd9f47fa63da008bd433954641686859f0d4b
Homomorpheus <e12225948@student.tuwien.ac.at>
2023-10-05 17:26:28 +0200

    added matrix class

---
+++ src/matrix.h
@@ -0,0 +1,59 @@
+#ifndef FILE_MATRIX_H
+#define FILE_MATRIX_H
+
+
+namespace ASC_bla
+{
+
+enum ORDERING {RowMajor, ColMajor};
+
+template <typename T, ORDERING ORD>
+class Matrix {
+    size_t height_;
+    size_t width_;
+    T* data_;
+
+    public:
+        Matrix (size_t height, size_t width): height_(height), width_(width), data_(new T[height*width]) {};
+        
+        Matrix (const Matrix & A): Matrix(A.height(), A.width()){*this = A;};
+
+        //move ctor
+
+        ~Matrix () { delete [] data_; }
+
+        Matrix & operator= (const Matrix & A2) {
+            for (size_t i = 0; i < height_; i++){
+                for(size_t j = 0; j < width_; j++ ){
+                    (*this)(i, j) = A2(i, j);};
+            }   
+            return *this;
+            }
+        //move operator=
+
+
+        size_t height () const {return this->height_;}; 
+
+        size_t width () const {return this->width_;};
+
+        const T & operator() (size_t i, size_t j) const {
+            if (ORD == RowMajor){
+                return data_[i*width_+j];
+            }
+            if (ORD == ColMajor){
+                return data_[j*height_+i];
+            }
+        }
+
+        T & operator() (size_t i, size_t j){
+            if (ORD == RowMajor){
+                return data_[i*width_+j];
+            }
+            if (ORD == ColMajor){
+                return data_[j*height_+i];
+            }
+        }
+};
+
+}
+#endif
---
+++ tests/test_matrix.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include <matrix.h>
+
+namespace bla = ASC_bla;
+
+
+int main()
+{ 
+  size_t m = 6;
+  size_t n = 5;
+  bla::Matrix<double, bla::RowMajor> A(m, n);
+  bla::Matrix<double, bla::ColMajor> B(m, n);
+  /*
+  for (size_t i = 0; i < x.Size(); i++)
+    {
+      x(i) = i;
+      y(i) = 10;
+    }
+
+  bla::Matrix<double> z = x+y;
+
+  std::cout << "x+y = " << z << std::endl;
+  */
+}
+
+
