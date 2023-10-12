050860d1bb658f04a0f9bb4dec66976525adec39
Homomorpheus <e12225948@student.tuwien.ac.at>
2023-10-12 13:26:36 +0200

    Alles Fertig Schatziiii

--- src/matrix.h
+++ src/matrix.h
@@ -1,6 +1,8 @@
 #ifndef FILE_MATRIX_H
 #define FILE_MATRIX_H
 
+#include <vector.h>
+
 namespace ASC_bla
 {
 
@@ -68,7 +70,67 @@ template <typename T, ORDERING ORD>
                 return data_[j * height_ + i];
             }
         }
+        
+        
     };
 
+    Vector operator* (Matrix& M, Vector& v) const
+    {
+        Vector<double> product(n);  //strichpunkte
+        size_t n = M.height();
+        size_t m = M.width();
+        double temp = 0;
+
+        if (m != v.Size()){break;}
+        
+        for(size_t i=0, i<n, i++){             //klammern???
+            for(size_t j=0, j<m, j++){
+                temp += (M(i,j) * v(j)); //klammer notwendig?
+            }
+            product(i) = temp;
+            temp = 0;
+        } 
+
+        return product;
+    }
+
+    Matrix operator* (Matrix& A, Matrix& B) const
+    {
+        Matrix<double> Product(n,l);  //strichpunkte
+        size_t n = A.height();
+        size_t m = A.width();
+        size_t l = B.width();
+        double temp = 0;
+
+        if (m != B.height()){break;}
+        
+        for(size_t i=0, i<l, i++){
+            for(size_t j=0, j<n, j++){             //klammern???
+                for(size_t k=0, k<m, k++){
+                    temp += (A(j,k) * B(k,i)); //klammer notwendig?
+                }
+                Product(j,i) = temp;
+                temp = 0;
+            } 
+        }
+            return Product;
+    }    
+
+    Matrix transp (Matrix& A)
+    {
+        Matrix<double> Transp(m,n);
+        size_t n = A.height();
+        size_t m = A.width();
+        
+        for(size_t i=0, i<n, i++){
+            for(size_t j=0, j<m, j++){
+                Transp(j,i) = A(i,j);
+            }
+        }   
+        
+        return Transp;
+
+    }
 }
+
 #endif
