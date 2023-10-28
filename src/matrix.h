dc19667065d3a76f051e48cc2c8c469b36bb855b
Homomorpheus <e12225948@student.tuwien.ac.at>
2023-10-28 12:07:17 +0200

    removed oldest matrix class, added detailed todo list

--- src/matrix.h
+++ src/matrix.h
@@ -11,11 +11,25 @@ enum ORDERING { RowMajor, ColMajor };
 
 template <typename T, ORDERING ORD>
 class MatrixView : public MatrixExpr<MatrixView<T, ORD> >
-{/*
+{
  protected:
   size_t height_, width_, dist_;
   T *data_;
 
+  // TODO constructor (with implementation of dist inside!)
+  // TODO copy constructor
+  // TODO operator=
+  // (TODO operator= for scalars)
+  // TODO View()
+  // TODO width(), height()
+  // TODO operator() (as & and as const &)
+  // TODO Row()
+  // TODO Col()
+  // TODO Rows()
+  // TODO Cols()
+  // TODO Transpose(MatrixView) (outside of MatrixView)
+  // TODO (MatrixView) (outside of MatrixView)
+/*
  public:
   // constr
   MatrixView(size_t height, size_t width, size_t dist; T *data)
@@ -75,7 +89,7 @@ class MatrixView : public MatrixExpr<MatrixView<T, ORD> >
     if (ORD == ColMajor) {
       return data_[j * dist_ + i];
     }
-  }
+  }*/
 };
 
 template <typename T, ORDERING ORD>
@@ -86,6 +100,13 @@ class Matrix : public MatrixView<T, ORD> {
   using BASE::dist_;
   using BASE::width_;
 
+  // (TODO move constructor)
+  // (TODO expression constructor)
+  // TODO destructor
+  // TODO operator=
+  // TODO move operator=
+  // TODO output stream operator
+/*
  public:
   Matrix(size_t height_, size_t width_)
     : MatrixView<T, ORD> (size, new T[data]) {
@@ -143,117 +164,6 @@ class Matrix : public MatrixView<T, ORD> {
   }*/
 };
 
-
-template <typename T, ORDERING ORD>
-class Matrix: public MatrixView<T, ORD>{ //TODO: should inherit from MatrixView!!!
-  size_t height_;
-  size_t width_;
-  T *data_;
- 
- /*
- public:
-  Matrix(size_t height, size_t width)
-      : height_(height), width_(width), data_(new T[height * width]){};
-
-  Matrix(const Matrix &A) : Matrix(A.height(), A.width()) { *this = A; };
-
-  // TODO move ctor
-
-  ~Matrix() { delete[] data_; }
-
-  Matrix &operator=(const Matrix &A2) {
-    for (size_t i = 0; i < height_; i++) {
-      for (size_t j = 0; j < width_; j++) {
-        (*this)(i, j) = A2(i, j);
-      };
-    }
-    return *this;
-  }
-  // TODO move operator=
-
-  size_t height() const { return this->height_; };
-  size_t width() const { return this->width_; };
-
-  // round bracket access operator
-  const T &operator()(size_t i, size_t j) const {
-    if (ORD == RowMajor) {
-      return data_[i * width_ + j];
-    }
-    if (ORD == ColMajor) {
-      return data_[j * height_ + i];
-    }
-  }
-
-  T &operator()(size_t i, size_t j) {
-    if (ORD == RowMajor) {
-      return data_[i * width_ + j];
-    }
-    if (ORD == ColMajor) {
-      return data_[j * height_ + i];
-    }
-  }
-};
-
-Vector operator*(Matrix &M, Vector &v) const {
-  Vector<double> product(n);  // strichpunkte
-  size_t n = M.height();
-  size_t m = M.width();
-  double temp = 0;
-
-  if (m != v.Size()) {
-    break;
-  }
-
-  for (size_t i = 0, i < n, i++) {  // klammern???
-    for (size_t j = 0, j < m, j++) {
-      temp += (M(i, j) * v(j));  // klammer notwendig?
-    }
-    product(i) = temp;
-    temp = 0;
-  }
-
-  return product;
-}
-
-Matrix operator*(Matrix &A, Matrix &B) const {
-  Matrix<double> Product(n, l);  // strichpunkte
-  size_t n = A.height();
-  size_t m = A.width();
-  size_t l = B.width();
-  double temp = 0;
-
-  if (m != B.height()) {
-    break;
-  }
-
-  for (size_t i = 0, i < l, i++) {
-    for (size_t j = 0, j < n, j++) {  // klammern???
-      for (size_t k = 0, k < m, k++) {
-        temp += (A(j, k) * B(k, i));  // klammer notwendig?
-      }
-      Product(j, i) = temp;
-      temp = 0;
-    }
-  }
-  return Product;
-}
-
-Matrix transp(Matrix &A) {
-  Matrix<double> Transp(m, n);
-  size_t n = A.height();
-  size_t m = A.width();
-
-  for (size_t i = 0, i < n, i++) {
-    for (size_t j = 0, j < m, j++) {
-      Transp(j, i) = A(i, j);
-    }
-  }
-
-  return Transp;
-}
-
-*/
-};
 
 }  // namespace ASC_bla
 
