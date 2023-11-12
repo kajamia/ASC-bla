d691b237f05a81ea10b5fe89c93cc5d3cf9d832b
neurogits <e12202053@student.tuwien.ac.at>
2023-11-12 23:51:18 +0100

    added LU-decomposition

--- src/lapack_interface.h
+++ src/lapack_interface.h
@@ -10,6 +10,7 @@
 
 
 #include <complex>
+#include <vector>
 
 typedef int integer;
 typedef integer logical;
@@ -27,7 +28,7 @@ typedef int L_fp;  // ?
 
 
 extern "C" {
-#include <clapack.h>
+#include "clapack.h"
 }
 
 
@@ -56,10 +57,16 @@ namespace ASC_bla
    //   throw std::runtime_error(std::string("daxpy returned errcode "+std::to_string(err)));      
   }
   
-  
+  // InnerProductLapack, NormLapack
+
+
   // BLAS-2 functions:
+  // MultMatVecLapack
+
+
 
   // BLAS-3 functions:
+  // MultMatMatLapack
   
   // int dgemm_ (char *transa, char *transb, integer *m, integer * n,
   // integer *k, doublereal *alpha, doublereal *a, integer *lda, 
@@ -108,32 +115,77 @@ namespace ASC_bla
 
   // | operator overload
 
-  
-/* 
+
+
+  //a.Height() - a.SizeRows()
+  //a.Width() - a.SizeCols()
+
   template <ORDERING ORD>
   class LapackLU {
     Matrix <double, ORD> a;
     std::vector<integer> ipiv;
-    
+
+
   public:
-    LapackLU (Matrix<double,ORD> _a)
-      : a(std::move(_a)), ipiv(a.Height()) {
-      integer m = a.Height();
-      if (m == 0) return;
-      integer n = a.Width();
-      integer lda = a.Dist();
-      integer info;
+    LapackLU (Matrix<double,ORD> _a, bool apply = true)
+    : a(std::move(_a)), ipiv(a.SizeRows()) {
+    //constructor, if flag apply true then decompose, if False decompose only when needed
+      if (apply){}
+        integer m = a.SizeRows();
+        if (m == 0) return;
+        integer n = a.SizeCols();
+        integer lda = a.Dist();
+        integer info;
     
-      // int dgetrf_(integer *m, integer *n, doublereal *a, 
+    
+      //int dgetrf_(integer *m, integer *n, doublereal *a, 
       //             integer * lda, integer *ipiv, integer *info);
 
       dgetrf_(&n, &m, &a(0,0), &lda, &ipiv[0], &info);
     }
     
+/*
+
+  // destructor
+    LapackLU() = default;
+
+  // Matrix getter
+    Matrix<double, ORD> GetMatrix() const { return a; }
+
+  // << operator
+    friend std::ostream& operator<<(std::ostream& os, const LapackLU<ORD>& lu) {
+      os << std::endl;
+      for (size_t i = 0; i < lu.a.SizeRows(); i++) {
+        for (size_t j = 0; j < lu.a.SizeCols(); j++) {
+          os << lu.a(i, j) << " ";
+        }
+        os << std::endl;
+      }
+      return os;
+    }
+
+    void Decompose(){
+        integer m = a.SizeRows();
+        if (m == 0) return;
+        integer n = a.SizeCols();
+        integer lda = a.Dist();
+        integer info;
+    
+    
+      //int dgetrf_(integer *m, integer *n, doublereal *a, 
+      //             integer * lda, integer *ipiv, integer *info);
+
+      dgetrf_(&n, &m, &a(0,0), &lda, &ipiv[0], &info);
+    }
+*/
+
+
+
+
     // b overwritten with A^{-1} b
     void Solve (VectorView<double> b) const {
       char transa =  (ORD == ColMajor) ? 'N' : 'T';
-      integer n = a.Height();
+      integer n = a.SizeRows();
       integer nrhs = 1;
       integer lda = a.Dist();
       integer ldb = b.Size();
@@ -146,32 +198,74 @@ namespace ASC_bla
       dgetrs_(&transa, &n, &nrhs, a.Data(), &lda, (integer*)ipiv.data(), b.Data(), &ldb, &info);
     }
   
+
     Matrix<double,ORD> Inverse() && {
-      double hwork;
-      integer lwork = -1;
-      integer n = a.Height();      
-      integer lda = a.Dist();
-      integer info;
-
-      // int dgetri_(integer *n, doublereal *a, integer *lda, 
-      //             integer *ipiv, doublereal *work, integer *lwork, 
-      //             integer *info);
-
-      // query work-size
-      dgetri_(&n, &a(0,0), &lda, ipiv.data(), &hwork, &lwork, &info);
-      lwork = integer(hwork);
-      std::vector<double> work(lwork);
-      dgetri_(&n, &a(0,0), &lda, ipiv.data(), &work[0], &lwork, &info);
-      return std::move(a);      
+    double hwork;
+    integer lwork = -1;
+    integer n = a.SizeRows();      
+    integer lda = a.Dist();
+    integer info;
+    /*
+    int dgetri_(integer *n, doublereal *a, integer *lda, 
+                integer *ipiv, doublereal *work, integer *lwork, 
+                integer *info);
+    */       
+    dgetri_(&n, &a(0,0), &lda, ipiv.data(), &hwork, &lwork, &info);
+    lwork = integer(hwork);
+    std::vector<double> work(lwork);
+    dgetri_(&n, &a(0,0), &lda, ipiv.data(), &lwork[0], &lwork, &info);
+    return std::move(a);  
+  
     }
 
-    // Matrix<double,ORD> LFactor() const { ... }
-    // Matrix<double,ORD> UFactor() const { ... }
-    // Matrix<double,ORD> PFactor() const { ... }
-  };
- */
-  
-}
+/*
+    Matrix<double,ORD> LFactor() const { ... }
+    Matrix<double,ORD> UFactor() const { ... }
+    Matrix<double,ORD> PFactor() const { ... }
+*/
+
+    Matrix<double, ORD> LFactor() const {
+      Matrix<double, ORD> L(a.SizeRows(), a.SizeCols());
+      for (size_t i = 0; i < a.SizeRows(); i++) {
+        for (size_t j = 0; j < a.SizeCols(); j++) {
+          if (i > j)
+            L(i, j) = a(i, j);
+          else if (i == j)
+            L(i, j) = 1;
+          else
+            L(i, j) = 0;
+        }
+      }
+      return L;
+    };
+
+    Matrix<double, ORD> UFactor() const {
+      Matrix<double, ORD> U(a.SizeRows(), a.SizeCols());
+      for (size_t i = 0; i < a.SizeRows(); i++) {
+        for (size_t j = 0; j < a.SizeCols(); j++) {
+          if (i <= j)
+            U(i, j) = a(i, j);
+          else
+            U(i, j) = 0;
+        }
+      }
+      return U;
+    };
+
+    Matrix<double, ORD> PFactor() const {
+      Matrix<double, ORD> P(a.SizeRows(), a.SizeCols());
+      for (size_t i = 0; i < a.SizeRows(); i++) {
+        for (size_t j = 0; j < a.SizeCols(); j++) {
+          if (i == tau[j])
+            P(i, j) = 1;
+          else
+            P(i, j) = 0;
+        }
+      }
+      return P;
+
+    };
+};
 
 
 #endif
