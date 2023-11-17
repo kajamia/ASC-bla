5ef4ebc380c024fba1368342bfa2a3537a547656
Homomorpheus <e12225948@student.tuwien.ac.at>
2023-11-17 14:08:26 +0100

    and another

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Kaja M., Vivien R., Daniel L., Norbert H.'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ["furo"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
